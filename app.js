d4376ea3434c21223d12ea4019b9dc82d4a0f383
hisamatsu yoshiyuki <hisasann25@gmail.com>
2020-05-16 21:03:18 +0900

    First express app

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+console.log(`port is ${port}`)
+
+app.get('/', (req, res) => res.send('Hello World!!'))
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
